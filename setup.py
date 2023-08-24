cb89946a5332d6388c83e16833e5ce7584a979b6
Xingyao Wang <xingyao6@illinois.edu>
2023-08-24 21:33:27 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jaxlm',
+    version='0.1',
+    packages=find_packages(),
+    author='Xingyao Wang',
+    author_email='xingyao6@illinois.edu',
+    description='A simplistic Jax-based Language Model Training and Serving Framework.',
+    url='https://github.com/xingyaoww/jaxlm',
+    license='MIT',
+)
